d01bb1e24251dd7c4c949cb8323d730fae961e95
Ursula Maria Mayer <ursula.maria.mayer@sciform.com>
2019-03-28 10:20:44 +0100

    clean up

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -24,27 +24,15 @@ class [[eosio::contract("hello")]] hello : public eosio::contract
 // handle the dispatching of actions for the contract
 EOSIO_DISPATCH( hello, (hi) )
 
-/*
-Actions implement the behaviour of a contract.
-Communication between contract actions comes in two flavours,
-inline actions, which will execute in the same transaction as the calling action, and
-deferred actions, which will be executed at a later time.
-*/
+
+
 
 // eosio-cpp abigen -o hello.wasm hello.cpp --abigen
 // eosio-cpp -o hello.wasm hello.cpp --abigen
 
-// check keys
-// cleos wallet keys
-
-// create an account for the contract
-// cleos create account eosio hello EOS8W2KziXjFek8JrGEAGntv7okauxxFR6fW9Ft1SfcxnDLKJafoM -p eosio@active
-
-// push action
-// cleos push action hello hi '["bob"]' -p bob@active
 
 /*
- * sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt update
 sudo apt upgrade
 apt install libstdc++6
